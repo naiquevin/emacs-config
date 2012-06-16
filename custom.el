03b918e1663bc5afc018b3e5a74bc3f7fd31e312
Vineet <naikvin@gmail.com>
2012-06-16 20:50:24 +0530

    custom changes made by emacs

--- custom.el
+++ custom.el
@@ -3,7 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("935e766f12c5f320c360340c8d9bc1726be9f8eb01ddeab312895487e50e5835" default)))
+ '(background-color "#002b36")
+ '(background-mode dark)
+ '(cursor-color "#839496")
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "935e766f12c5f320c360340c8d9bc1726be9f8eb01ddeab312895487e50e5835" default)))
+ '(foreground-color "#839496")
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
