b56484cab05ceff3db43654dc2ba1d1534d660a9
Vineet Naik <vineet.naik@kodeplay.com>
2012-11-23 15:10:26 +0530

    Add hook to load jinja2 mode for templates under python projects

--- init.el
+++ init.el
@@ -91,9 +91,20 @@
 (add-to-list 'flymake-allowed-file-name-masks '("\\.php$" flymake-php-init))
 
 
-;; Jinja2 Templates
+;;; Jinja2 Templates
 (require 'jinja2-mode)
-;; Needs to be selectively activated in html file using C-c j
+
+;; For all .html under /python/../templates/, jinja2-mode activated on
+;; find-file
+(add-hook
+ 'find-file-hook
+ (lambda ()
+   (when (and (string-match-p "/python/" (buffer-file-name))
+              (string-match-p "/templates/" (buffer-file-name)))
+     (jinja2-mode))))
+
+;; Needs to be selectively activated in case of other html file using
+;; C-c j
 (define-key html-mode-map (kbd "C-c j") 'jinja2-mode)
 
 
