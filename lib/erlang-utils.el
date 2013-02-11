77d01f4b46ade4be19c21db08549ba35070234d1
Vineet Naik <vineet.naik@kodeplay.com>
2013-02-11 18:23:36 +0530

    Open erlang application resource files (*.app and *.app.src) in erlang mode

--- lib/erlang-utils.el
+++ lib/erlang-utils.el
@@ -5,6 +5,7 @@
  (lambda ()
    ;; scheme's equivalent of python's `any`?
    (when (or (string-match "/ebin/.*\.app$" (buffer-file-name))
+             (string-match "/src/.*\.app.src$" (buffer-file-name))
              (string-match "Emakefile" (buffer-name))
              (string-match "rebar.config" (buffer-name)))
      (erlang-mode))))
