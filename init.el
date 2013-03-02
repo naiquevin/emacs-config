d4bf846a04146ed277bcb416e92d7f28eed07b93
Vineet Naik <vineet.naik@kodeplay.com>
2013-03-02 16:21:20 +0530

    Add git modes for easy editing of git files

--- init.el
+++ init.el
@@ -113,6 +113,12 @@
 (define-key html-mode-map (kbd "C-c j") 'jinja2-mode)
 
 
+;; Git modes for editing git based buffers
+;; https://github.com/lunaryorn/git-modes
+(require 'git-commit-mode)
+(require 'gitignore-mode)
+(require 'gitconfig-mode)
+
 ;;;; Editing and Outlining
 
 ;;; Markdown mode
