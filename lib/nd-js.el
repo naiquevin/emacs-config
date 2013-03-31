2eb42e8d23e96536ed5e27392566776d4c457736
Vineet <naikvin@gmail.com>
2013-03-31 17:37:32 +0530

    Add nd-js.el for natural docs style docs for js

---
+++ lib/nd-js.el
@@ -0,0 +1,124 @@
+
+
+(defun nd-js-doc ()
+  (interactive)
+  (when (string= major-mode "js-mode")
+    (let ((jsdef (nd-js-match-def (current-line-string))))
+      (if jsdef
+          (progn 
+            (previous-line)
+            (newline-and-indent)
+            (let ((type (car jsdef))
+                  (name (cadr jsdef))
+                  (args (cadr (cdr jsdef))))
+              (insert "/**")
+              (insert (nd-js-commentize (nd-js-doc-title type name) t))
+              (insert (nd-js-commentize (format "<describe %s>" name) t))
+              (when args
+                (insert (nd-js-commentize "Parameters:" t))
+                (dolist (a args)
+                  (insert (nd-js-commentize (format "  (<type>) %s - <describe %s>" a a) t)))
+                (insert (nd-js-commentize "Returns:"))
+                (insert (nd-js-commentize "  <describe return type>")))
+              (insert "\n */")
+              (nd-js-indent-comment-region)))
+        (message "Not a valid javascript definition")))))
+
+
+(global-set-key (kbd "C-c d") 'nd-js-doc)
+
+
+(defun nd-js-commentize (text &optional newline)
+  (if newline
+      (format "\n * %s\n *" text)
+    (format "\n * %s" text)))
+
+
+(defun nd-js-indent-comment-region ()
+  (search-backward "/**")
+  (move-beginning-of-line nil)
+  (let ((beg (point)))
+    (search-forward "*/")
+    (move-end-of-line nil)
+    (indent-region beg (point))))
+
+
+(defun nd-js-doc-title (type name)
+  (format "%s: %s" (capitalize type) name))
+
+
+(defun current-line-string ()
+  (interactive)
+  (buffer-substring (point-at-bol) (point-at-eol)))
+
+
+(defun nd-js-func-args (argstr)
+  (if (string= argstr "")
+      nil
+    (mapcar (lambda (str)
+              (replace-regexp-in-string 
+               "\\`[ \t\n]*" "" 
+               str))
+            (split-string  argstr ","))))
+
+
+(defun nd-js-match-def (string)
+  (cond ((string-match "^var\s?\\([a-zA-Z0-9_]+\\)\s?=\s?function\s?(\\(.*\\))\s?{" string)
+         (list "function" 
+               (match-string 1 string)
+               (nd-js-func-args (match-string 2 string))))
+        ((string-match "^function\s?\\([a-zA-Z0-9_]+\\)\s?(\\(.*\\))\s?{" string)
+         (list "function" 
+               (match-string 1 string)
+               (nd-js-func-args (match-string 2 string))))
+        ((string-match "\\([a-zA-Z0-9_]+\\):\s?function\s?(\\(.*\\))\s?{" string)
+         (list "function"
+               (match-string 1 string)
+               (nd-js-func-args (match-string 2 string))))
+        ((string-match "^var\s?\\([a-zA-Z0-9_]+\\)\s?" string)
+         (list "variable" (match-string 1 string) nil))
+        ((string-match "\\([a-zA-Z0-9_]+\\)\s?:\s?.*,?" string)
+         (list "variable" (match-string 1 string) nil))))
+
+
+
+(defun nd-js-tests ()
+  ;; tests for nd-js-func-args
+  (assert (equal (nd-js-func-args "x, y") '("x" "y")))
+  (assert (equal (nd-js-func-args "") nil))
+
+  ;; tests for nd-js-match-def
+  (assert (equal (nd-js-match-def "var helloworld = 42;")
+                 '("variable" "helloworld" nil)))
+  (assert (equal (nd-js-match-def "var hello_world = function (x, y) {")
+                 '("function" "hello_world" ("x" "y"))))
+  (assert (equal (nd-js-match-def "function hw901_88 () {")
+                 '("function" "hw901_88" nil)))
+  (assert (equal (nd-js-match-def "helloWorld: 42")
+                 '("variable" "helloWorld" nil)))
+  (assert (equal (nd-js-match-def "helloworld: function (x, y) {")
+                 '("function" "helloworld" ("x" "y"))))
+
+  ;; tests with whitespace inconsistencies
+  (assert (equal (nd-js-match-def "var helloworld=42;")
+                 '("variable" "helloworld" nil)))
+  (assert (equal (nd-js-match-def "var hello_world = function(x,y){")
+                 '("function" "hello_world" ("x" "y"))))
+  (assert (equal (nd-js-match-def "function hw901_88(){")
+                 '("function" "hw901_88" nil)))
+  (assert (equal (nd-js-match-def "helloWorld : 42,")
+                 '("variable" "helloWorld" nil)))
+  (assert (equal (nd-js-match-def "helloworld:function (x,y) { ")
+                 '("function" "helloworld" ("x" "y"))))
+
+  ;; tests for non-relevant lines (should return nil)
+  (assert (equal (nd-js-match-def "x = y + 1;") nil))
+  
+
+  (assert (string= (nd-js-doc-title "variable" "hello") "Variable: hello"))
+  (assert (string= (nd-js-doc-title "function" "doThis") "Function: doThis"))
+  )
+
+
+;; (nd-js-tests)
+
