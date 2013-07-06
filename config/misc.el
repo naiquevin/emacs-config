1608e7879e990c7762279099a730efcf971b5d90
Vineet <naikvin@gmail.com>
2013-07-06 19:37:46 +0530

    Enable gfm-mode for README.md files in git repos

--- config/misc.el
+++ config/misc.el
@@ -22,6 +22,17 @@
 (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
 
+;; Enable gfm-mode (github-flavoured-markdown) if it's a README.md
+;; file inside of a git repository
+(add-hook
+ 'find-file-hook
+ (lambda ()
+   (when (and (string-match "README.md" (buffer-name))
+              (file-exists-p
+               (concat (file-name-directory (buffer-file-name)) ".git")))
+     (gfm-mode))))
+
+
 ;; Org mode
 (load "config/org-config.el")
 
