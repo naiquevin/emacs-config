dd0009c567729bb833eca12ac0ea07b9b9b38072
Vineet <naikvin@gmail.com>
2013-08-11 00:23:25 +0530

    Remove jinja2-mode.el and install via elpa
    Also removed the key binding to enable jinja-mode in html buffers since
    I didn't really use it

--- config/html-config.el
+++ config/html-config.el
@@ -1,5 +1,4 @@
 ;;; Jinja2 Templates
-(require 'jinja2-mode)
 
 ;; For all .html under /python/../templates/, jinja2-mode activated on
 ;; find-file
@@ -9,8 +8,4 @@
    (when (and (string-match-p "/python/" (buffer-file-name))
               (string-match-p "/templates/" (buffer-file-name)))
      (jinja2-mode))))
-
-;; Needs to be selectively activated in case of other html file using
-;; C-c j
-(define-key html-mode-map (kbd "C-c j") 'jinja2-mode)
 
