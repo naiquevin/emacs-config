cd0ccb7f36d4215c0e97024ae0345354f0788ac1
Vineet <naikvin@gmail.com>
2013-08-15 21:48:29 +0530

    Removed code for including org mode as it's built-in now

--- config/org-config.el
+++ config/org-config.el
@@ -1,10 +1,5 @@
 ;;; My OrgMode Setting
 
-(setq load-path (cons "~/emacs/site/org-mode/lisp" load-path))
-(setq load-path (cons "~/emacs/site/org-mode/contrib/lisp" load-path))
-(require 'org)
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
