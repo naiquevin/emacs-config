99a703db0f2a1fee11682270a9e6e1cadcba210e
Vineet <naikvin@gmail.com>
2013-08-15 20:17:38 +0530

    Install markdown-mode via package.el
    * Removed the old lib/markdown-mode.el file
    * Updated the requirements.txt file

--- config/misc.el
+++ config/misc.el
@@ -17,7 +17,7 @@
 (require 'gitconfig-mode)
 
 ;; Markdown mode
-(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t) 
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
 
