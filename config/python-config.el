324cfdccb515154a39d0ece87733f1ecd4c86f2f
Vineet <naikvin@gmail.com>
2013-08-26 22:14:31 +0530

    Enable ipython, rainbow-delimiters and eldoc for python

--- config/python-config.el
+++ config/python-config.el
@@ -12,3 +12,23 @@
   
   (add-to-list 'flymake-allowed-file-name-masks 
                '("\\.py\\'" flymake-pyflakes-init)))
+
+
+;; Set ipython as the python interpreter
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args "--colors NoColor"
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+
+;; Enable rainbow-delimiters in python and inferior modes
+(add-hook 'python-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'inferior-python-mode-hook 'rainbow-delimiters-mode)
+
+;; Enable eldoc only in inferior-python-mode
+(add-hook 'inferior-python-mode-hook 'turn-on-eldoc-mode)
+
