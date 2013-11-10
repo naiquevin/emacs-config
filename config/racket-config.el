4d8d2612d1cdca8769fbb3081c53a5b3a1201a12
Vineet <naikvin@gmail.com>
2013-11-10 23:44:37 +0530

    Added some tweaks for racket development
    * Enabled paredit mode for racket buffers
    * Disabled quack-find-file overriding find-file

--- config/racket-config.el
+++ config/racket-config.el
@@ -2,8 +2,15 @@
 
 ;; Quack (Racket)
 (require 'quack)
+
+; use emacs font style instead of the default drracket one
 (setq quack-fontify-style 'emacs)
+
+; don't override find-file, ido is good
+(setq quack-remap-find-file-bindings-p nil)
 
 ;; Rainbow delimiters (for scheme)
 (add-hook 'scheme-mode-hook 'rainbow-delimiters-mode)
 
+;; Paredit mode
+(add-hook 'scheme-mode-hook 'enable-paredit-mode)
