ffef3602798d758a010fd6c00ac2297eb15ce4ca
Vineet <naikvin@gmail.com>
2014-01-10 22:13:10 +0530

    Upgrade from nrepl to cider

--- config/clojure-config.el
+++ config/clojure-config.el
@@ -2,12 +2,15 @@
 
 ;; Enable rainbow delimeters in clojure and nrepl buffers
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 
 ;; Turn on eldoc mode
 (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
 
 ;; Turn on paredit mode on clojure and nrepl modes
 (add-hook 'clojure-mode-hook 'enable-paredit-mode)
-(add-hook 'nrepl-mode-hook 'enable-paredit-mode)
+(add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
+
+;; Prevent nrepl connection buffers appearing while buffer switching
+(setq nrepl-hide-special-buffers t)
 
