ca7c7c432324fc4fa7b6bde37cf6d7fde3ca1e40
Vineet Naik <vineet.naik@kodeplay.com>
2014-01-18 18:07:08 +0530

    Add and configure multiple-cursors

--- config/misc.el
+++ config/misc.el
@@ -50,3 +50,14 @@
 ;; Toggle window split
 (require 'toggle-window-split)
 
+
+;; Multiple cursors
+
+(add-hook 'after-init-hook
+          (lambda ()
+            (progn
+              (require 'multiple-cursors)
+              (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+              (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+              (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+              (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))))
