3549b2c4d493c0834584c379c0911fb53079f6c2
Vineet Naik <vineet.naik@kodeplay.com>
2014-05-06 11:39:25 +0530

    Prefer utf-8 for everything

--- config/general-config.el
+++ config/general-config.el
@@ -23,6 +23,20 @@
 (show-paren-mode t)                   ; Show matching paren
 
 
+;; prefer everything in utf-8
+(prefer-coding-system 'utf-8)
+(set-language-environment 'UTF-8)
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(setq file-name-coding-system 'utf-8
+      buffer-file-coding-system 'utf-8
+      coding-system-for-write 'utf-8
+      default-process-coding-system '(utf-8 . utf-8))
+
+
 ;; ido-mode config and preferences
 (ido-mode t)
 (setq ido-enable-prefix nil
