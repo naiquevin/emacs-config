f40cc970e7d838671bff52b594b88e2f46b8f192
Vineet <naikvin@gmail.com>
2014-05-19 23:36:10 +0530

    Fix find-file-hook to enable jinja2-mode
    The bug was that it enabled jinja2-mode for any file inside the
    'templates' dir in python project and not just a '.html' file.

--- config/html-config.el
+++ config/html-config.el
@@ -1,12 +1,12 @@
 ;;; Jinja2 Templates
-;; (require 'jinja2-mode)
 
 ;; For all .html under /python/../templates/, jinja2-mode activated on
 ;; find-file
 (add-hook
  'find-file-hook
  (lambda ()
-   (when (and (string-match-p "/python/" (buffer-file-name))
+   (when (and (string= (file-name-extension (buffer-file-name)) "html")
+              (string-match-p "/python/" (buffer-file-name))
               (string-match-p "/templates/" (buffer-file-name)))
      (jinja2-mode))))
 
