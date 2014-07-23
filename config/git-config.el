2145fdc1aa36d397314a632ebc247d0fd5b88582
Vineet Naik <vineet.naik@kodeplay.com>
2014-07-23 13:30:24 +0530

    Modify magit faces for solarized dark theme

--- config/git-config.el
+++ config/git-config.el
@@ -2,3 +2,15 @@
 
 ;; magit
 (global-set-key (kbd "C-c M-g") 'magit-status)
+
+
+;; Modify default magit faces to look good on solarized dark theme
+(add-hook 'magit-mode-hook
+          (lambda ()
+            (set-face-foreground 'magit-log-sha1 "#b58900")
+            (set-face-foreground 'magit-log-author "#cb4b16")
+            (set-face-foreground 'magit-log-date "#2aa198")
+
+            (set-face-attribute 'magit-branch nil
+                                :background 'unspecified
+                                :foreground "#268bd2")))
