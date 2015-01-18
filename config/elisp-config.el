899495886d1b5151dad2526e265a8dbd8db9e1fc
Vineet Naik <vineet@helpshift.com>
2015-01-18 12:51:00 +0530

    Enable paredit in ielm buffers

--- config/elisp-config.el
+++ config/elisp-config.el
@@ -3,6 +3,7 @@
 ;; Rainbow delimiters (for emacs lisp)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
-;; Paredit mode for emacs lisp
+;; Paredit mode for emacs lisp mode and ielm
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'ielm-mode-hook 'enable-paredit-mode)
 
