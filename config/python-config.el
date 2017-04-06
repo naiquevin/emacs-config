7671635f24bf15cf6ac8ee3ba07f1ec7f45f102d
Vineet Naik <vineet@helpshift.com>
2017-04-06 20:57:58 +0530

    Python config to work with new ipython

--- config/python-config.el
+++ config/python-config.el
@@ -15,7 +15,7 @@
 ;; Set ipython as the python interpreter
 (setq
  python-shell-interpreter "ipython"
- python-shell-interpreter-args "--colors NoColor"
+ python-shell-interpreter-args "--colors NoColor --simple-prompt"
  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
  python-shell-completion-setup-code "from IPython.core.completerlib import module_completion"
@@ -25,7 +25,6 @@
 
 ;; Enable rainbow-delimiters in python-mode
 (add-hook 'python-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'inferior-python-mode-hook 'rainbow-delimiters-mode)
 
 
 ;; Autocomplete/Jedi Setup
