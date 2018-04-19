92db36e7efd22f3ec0ac94852fd5b12c075fc112
Vineet Naik <vineet@helpshift.com>
2018-04-19 19:22:40 +0530

    Commit changes to custom.el

--- custom.el
+++ custom.el
@@ -6,9 +6,16 @@
  '(background-color "#002b36")
  '(background-mode dark)
  '(cursor-color "#839496")
- '(custom-safe-themes (quote ("27b53b2085c977a8919f25a3a76e013ef443362d887d52eaa7121e6f92434972" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "935e766f12c5f320c360340c8d9bc1726be9f8eb01ddeab312895487e50e5835" default)))
+ '(custom-safe-themes
+   (quote
+    ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "27b53b2085c977a8919f25a3a76e013ef443362d887d52eaa7121e6f92434972" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "935e766f12c5f320c360340c8d9bc1726be9f8eb01ddeab312895487e50e5835" default)))
  '(foreground-color "#839496")
- '(quack-programs (quote ("mzscheme" "bigloo" "csi" "csi -hygienic" "gosh" "gracket" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "racket" "racket -il typed/racket" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi")))
+ '(package-selected-packages
+   (quote
+    (s groovy-mode cider clojure-mode org-super-agenda yasnippet yaml-mode virtualenvwrapper unbound sml-mode scss-mode scala-mode2 rainbow-delimiters pytest pydoc projectile php-mode persistent-scratch paredit nodejs-repl nginx-mode multiple-cursors material-theme markdown-mode magit load-dir ledger-mode json-mode js2-mode jinja2-mode jedi gradle-mode go-mode gitignore-mode gitconfig-mode geiser flycheck flx-ido ess erlang bongo bats-mode async ansible-doc android-mode ace-window)))
+ '(quack-programs
+   (quote
+    ("mzscheme" "bigloo" "csi" "csi -hygienic" "gosh" "gracket" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "racket" "racket -il typed/racket" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi")))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -16,4 +23,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ )
