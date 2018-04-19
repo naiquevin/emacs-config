b81a2983a9926aa738ae8ca495582e497158f728
Vineet Naik <vineet@helpshift.com>
2018-04-19 19:21:45 +0530

    Add melpa stable to list of package-archives

--- pkg-init.el
+++ pkg-init.el
@@ -9,7 +9,8 @@
 ;; Set package archive sources
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (package-initialize)
 
