1803cb2036111fa94b8979a525ed30c5bf5ab981
Vineet Naik <vineet@helpshift.com>
2018-04-19 19:26:22 +0530

    Improve org-mode config

--- config/org-config.el
+++ config/org-config.el
@@ -24,14 +24,36 @@
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "gtd.org" "Tasks")
          "* TODO %?\n %i\n")
-        ("i" "Idea" entry (file "ideas.org")
-         "* %?\n")))
+        ("i" "Idea" entry (file+headline "gtd.org" "Ideas")
+         "* %?\n")
+        ("l" "Link" entry (file+headline "gtd.org" "Links")
+         "* %?\n %i\n")
+
+        ;; Helpshift specific prefixed keys
+        ("h" "Templates for Helpshift tasks")
+        ("ht" "Task" entry (file+olp "work.org" "Todo @ Helpshift" "Tasks")
+         "* TODO %?\n %i - %a\n")))
 
 ;; Setup refile targets
 (setq org-refile-targets
-      '((nil :maxlevel . 2) ; top level headlines in current buffer
-        (org-agenda-files :maxlevel . 2))) ; top level headlines in
-                                           ; other agenda files
+      ;; top level headlines in current buffer
+      '((nil :maxlevel . 9)
+        ;; top level headlines in other agenda files
+        (org-agenda-files :maxlevel . 9)))
+
+(setq org-outline-path-complete-in-steps nil)
+(setq org-refile-use-outline-path t)
+
+(setq org-completion-use-ido t)
+
+;; TODO faces
+(setq org-todo-keyword-faces
+      '(("TODO" :foreground "#dc322f" :weight bold)
+        ("DONE" :foreground "#859900" :weight bold)
+        ("WIP" :foreground "#b58900" :weight bold)
+        ("BLOCKED" :foreground "#cb4b16" :weight bold)
+        ("PARKED" :foreground "#586e75" :weight bold)
+        ("IMPROV" :foreground "#268bd2" :weight bold)))
 
 ;; enable export to markdown
 (eval-after-load "org"
