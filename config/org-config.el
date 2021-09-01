b85a3f7a0a5f891f60855fabe804e4e006106286
Vineet Naik <vineet@helpshift.com>
2021-09-01 16:25:31 +0530

    Install and configure org-roam

--- config/org-config.el
+++ config/org-config.el
@@ -81,3 +81,20 @@
 (org-crypt-use-before-save-magic)
 (setq org-tags-exclude-from-inheritance '("crypt"))
 (setq org-crypt-key "4096R/4930D5CB")
+
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory "~/Dropbox/RoamNotes")
+  :init
+  (setq org-roam-v2-ack t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (org-roam-setup)
+  (org-roam-db-autosync-mode))
