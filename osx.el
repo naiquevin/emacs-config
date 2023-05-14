2ac1630c596caec9f337d00eaf401072f40e4579
Vineet Naik <naikvin@gmail.com>
2023-05-14 16:37:23 +0530

    Support docs encrypted with older version of gpg

--- osx.el
+++ osx.el
@@ -46,8 +46,9 @@
 
 (setq locate-command "mdfind")
 
-;; Use gpg (GnuPG) 1.4.18. `gpg` was upgraded during last system upgrade
-(custom-set-variables '(epg-gpg-program "/opt/homebrew/bin/gpg"))
+(custom-set-variables '(epg-gpg-program "/usr/local/bin/gpg"))
+(setq epa-pinentry-mode 'loopback)
+
 
 (provide 'osx)
 
