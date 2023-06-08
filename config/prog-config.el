2ff5d4a4a41a3e37d3ff5ee6ba4746baa28ad661
Vineet Naik <naikvin@gmail.com>
2023-06-08 09:55:49 +0530

    Move flycheck config to common programming config file
    Also redefine the config using 'use-package' macro.

--- config/prog-config.el
+++ config/prog-config.el
@@ -81,3 +81,26 @@
   (setq plantuml-default-exec-mode 'executable)
   ;; Until the problem with svg is fixed - https://emacs.stackexchange.com/a/74504
   (plantuml-set-output-type "png"))
+
+
+(use-package flycheck
+  :ensure t
+
+  :config
+  ;; Disable the error indicator on the fringe
+  (setq flycheck-indication-mode 'left-fringe)
+  ;; disable automatic syntax checks on new line
+  (setq flycheck-check-syntax-automatically '(save
+                                              idle-change
+                                              mode-enabled))
+  ;; Immediate syntax checking quite annoying. Slow it down a bit.
+  (setq flycheck-idle-change-delay 2.0)
+  ;; Customize faces (Colors are copied from solarized definitions)
+  (set-face-attribute 'flycheck-warning nil
+                      :background "#b58900"
+                      :foreground "#262626"
+                      :underline nil)
+  (set-face-attribute 'flycheck-error nil
+                      :background "#dc322f"
+                      :foreground "#262626"
+                      :underline nil))
--- pkg-init.el
+++ pkg-init.el
@@ -31,7 +31,7 @@
 ;; DEPRECATED! Following code will be replaced with use-package
 
 (defvar pkg-init-packages
-  '(s dash load-dir ess flycheck geiser php-mode
+  '(s dash load-dir ess geiser php-mode
       ctable concurrent go-mode
       deferred jinja2-mode js2-mode nodejs-repl json-mode
       bats-mode
