60ac942e229bf7f20ccfea0bb078ee2d4ec920c7
Vineet Naik <naikvin@gmail.com>
2023-06-09 10:02:11 +0530

    Remove the deprecated approach of installing packages
    All packages are now using 'use-package'.

--- pkg-init.el
+++ pkg-init.el
@@ -14,8 +14,6 @@
                          ("org" . "http://orgmode.org/elpa/")))
 
 
-;; (require 'use-package)
-
 ;; Bootstrap 'use-package'
 (eval-after-load 'gnutls
   '(add-to-list 'gnutls-trustfiles "/etc/ssl/cert.pem"))
@@ -26,30 +24,6 @@
   (require 'use-package))
 (require 'bind-key)
 (setq use-package-always-ensure t)
-
-
-;; DEPRECATED! Following code will be replaced with use-package
-
-(defvar pkg-init-packages
-  '()
-  "A list of packages to ensure are installed at launch")
-
-
-(defun pkg-init-packages-installed-p ()
-  (cl-loop for p in pkg-init-packages
-	   when (not (package-installed-p p)) do (cl-return nil)
-	   finally (cl-return t)))
-
-
-(unless (pkg-init-packages-installed-p)
-  ;; check for new packages (package versions)
-  (message "%s" "Emacs is now refreshing its package database...")
-  (package-refresh-contents)
-  (message "%s" " done.")
-  ;; install the missing packages
-  (dolist (p pkg-init-packages)
-    (when (not (package-installed-p p))
-      (package-install p))))
 
 
 (provide 'pkg-init)
