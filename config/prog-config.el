703a2cc8a17c2498efd35decf2b2a04d19dd0eed
Vineet Naik <naikvin@gmail.com>
2023-06-09 09:24:30 +0530

    Move erlang config from a separate file to prog-config.el

--- config/prog-config.el
+++ config/prog-config.el
@@ -143,6 +143,20 @@
   (ruby-mode . filladapt-mode))
 
 
+(use-package erlang
+  :ensure t
+  :init
+  (add-hook
+   'find-file-hook
+   (lambda ()
+     ;; scheme's equivalent of python's `any`?
+     (when (or (string-match "/ebin/.*\.app$" (buffer-file-name))
+               (string-match "/src/.*\.app.src$" (buffer-file-name))
+               (string-match "Emakefile" (buffer-name))
+               (string-match "rebar.config" (buffer-name)))
+       (erlang-mode)))))
+
+
 (use-package go-mode
   :ensure t)
 
