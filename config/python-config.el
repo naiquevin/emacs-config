e06173b92179e35454c2e268a5306de0e194a4df
Vineet Naik <naikvin@gmail.com>
2023-06-16 09:36:30 +0530

    Add a way to activate a virtualenv inside a project
    In many python projects, the virtualenv is created at '.venv' inside
    the project root. The virtualenvwrapper package has support for
    automatically such virtualenvs by configuring the
    'projectile-switch-project-action' variable, however it doesn't seem
    to work with 'counsel-projectile'.
    
    On the other hand 'venv-workon' only works on virtualenvs created
    inside the 'venv-location' dir. So there's no way to activate a
    virtualenv that's created inside the project.
    
    This commit adds an interactive function that acts as a wrapper over
    'venv'workon'. It checks if a dir '.venv' exists in the dir returned
    by 'projectile-project-root'. In that case, it activates it. If not,
    it falls back to calling 'venv-workon' normally.
    
    This new function is bound to 'C-c v' in the python-mode-map.

--- config/python-config.el
+++ config/python-config.el
@@ -10,6 +10,39 @@
                 (append mode-line-format '((:exec venv-current-name)))))
 
 
+(defun naiq/venv-activate ()
+  "Wrapper to activate a virtualenv. Supports activating a
+virtualenv by `venv-lookup-names` in the projectile root dir or a
+`venv-location` (as a fallback)
+
+Depends on packages:
+
+  - virtualenvwrapper
+  - projectile
+  - ivy
+"
+  (interactive)
+  (let ((path (concat (projectile-project-root) ".venv")))
+    (if (file-exists-p path)
+        (ivy-read "Virtualenv found inside the project:" (cons path '())
+                  :action (lambda (vdir)
+                            ;; @NOTE: The following code is copied
+                            ;; from `venv-workon` implementation
+                            ;; inside virtualenvwrapper.el as it
+                            ;; doesn't provide a function to activate
+                            ;; a virtualenv by dir
+                            (let ((vname (venv-dir-to-name vdir)))
+                              ;; deactivate
+                              (venv-deactivate)
+                              ;; then switch
+                              (setq venv-current-name vname)
+                              ;; push it onto the history
+                              (add-to-list 'venv-history venv-current-name)
+                              ;; actually activate it
+                              (venv--activate-dir vdir))))
+      (venv-workon))))
+
+
 (use-package pytest
   :ensure t
   :bind ("C-c C-y" . pytest-module))
@@ -38,7 +71,12 @@
 (use-package python
   :ensure t
 
-  :after (smartparens rainbow-delimiters sphinx-doc flycheck-eglot)
+  :after (smartparens
+          rainbow-delimiters
+          sphinx-doc
+          flycheck-eglot
+          projectile
+          virtualenvwrapper)
 
   :config
   ;; Set ipython as the python interpreter
@@ -53,6 +91,7 @@
 
   (add-to-list 'eglot-server-programs
                '(python-mode . ("jedi-language-server")))
+  (diminish 'python " Py")
 
   :hook
   (python-mode . electric-pair-mode)
@@ -61,4 +100,7 @@
   (python-mode . rainbow-delimiters-mode)
   (python-mode . sphinx-doc-mode)
   (python-mode . eglot-w-flycheck)
-  (python-mode . company-python))
+  (python-mode . company-python)
+
+  :bind
+  (("C-c v" . naiq/venv-activate)))
