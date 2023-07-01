757e3c25fe3c20aeb5794a3a461f762f7cefcc1f
Vineet Naik <naikvin@gmail.com>
2023-07-01 19:32:59 +0530

    Define org config with use-package

--- config/org-config.el
+++ config/org-config.el
@@ -1,87 +1,95 @@
 ;;; My OrgMode Setting
 
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-(global-set-key "\C-co" 'org-mark-ring-goto)
+(use-package org
+  :config
+  ;; Basics
+  (setq org-directory "~/Dropbox/org")
+  (setq org-hide-leading-stars t)       ; hide leading stars
+  (setq org-adapt-indentation t) ; indent relative to the outline level
+  (setq org-log-done t)   ; display time when TODO was changed to DONE
 
-(global-set-key (kbd "s-<f11>") 'org-clock-goto)
+  ;;---------------------------------------------------------
 
-(setq org-hide-leading-stars t) ; hide leading stars
-(setq org-adapt-indentation t)  ; indent relative to the outline level
-(setq org-log-done t) ; display time when TODO was changed to DONE
+  ;; Appearance related config
+  (setq org-todo-keyword-faces
+        '(("TODO" :foreground "#dc322f" :weight bold)
+          ("DONE" :foreground "#859900" :weight bold)
+          ("WIP" :foreground "#b58900" :weight bold)
+          ("BLOCKED" :foreground "#cb4b16" :weight bold)
+          ("PARKED" :foreground "#586e75" :weight bold)
+          ("IMPROV" :foreground "#268bd2" :weight bold)
+          ("NEXT" :foreground "#b58900" :weight bold)))
 
-;; Customizing the org directory
-(setq org-directory "~/Dropbox/org")
+  ;;---------------------------------------------------------
 
-;; Agenda files
-(setq org-agenda-files (mapcar (lambda (x)
-                                 (concat org-directory "/"  x))
-                               '("gtd.org"
-                                 "work.org"
-                                 "sideprojects.org"
-                                 "ideas.org")))
+  ;; Agenda related config
+  (setq org-agenda-files (mapcar (lambda (x)
+                                   (concat org-directory "/"  x))
+                                 '("gtd.org"
+                                   "work.org"
+                                   "sideprojects.org"
+                                   "ideas.org")))
 
-;; Org Capture
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "gtd.org" "Tasks")
-         ;; Note that the 2 spaces before `%i` help in aligning the content
-         "* TODO %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n  %i\n")
-        ("i" "Idea" entry (file+headline "gtd.org" "Ideas")
-         "* %?\n")
-        ("l" "Link" entry (file+headline "gtd.org" "Links")
-         "* %?\n %i\n")
+  ;; Org Capture
+  (setq org-capture-templates
+        '(("t" "Todo" entry (file+headline "gtd.org" "Tasks")
+           ;; Note that the 2 spaces before `%i` help in aligning the content
+           "* TODO %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n  %i\n")
+          ("i" "Idea" entry (file+headline "gtd.org" "Ideas")
+           "* %?\n")
+          ("l" "Link" entry (file+headline "gtd.org" "Links")
+           "* %?\n %i\n")
 
-        ;; Helpshift specific prefixed keys
-        ("h" "Templates for Helpshift tasks")
-        ("ht" "Task" entry (file+olp "work.org" "Todo @ Helpshift" "Tasks")
-         "* TODO %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n  %i - %a\n")))
+          ;; Helpshift specific prefixed keys
+          ("h" "Templates for Helpshift tasks")
+          ("ht" "Task" entry (file+olp "work.org" "Todo @ Helpshift" "Tasks")
+           "* TODO %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n  %i - %a\n")))
 
-;; Setup refile targets
-(setq org-refile-targets
-      ;; top level headlines in current buffer
-      '((nil :maxlevel . 9)
-        ;; top level headlines in other agenda files
-        (org-agenda-files :maxlevel . 9)))
+  ;; Setup refile targets
+  (setq org-refile-targets
+        ;; top level headlines in current buffer
+        '((nil :maxlevel . 9)
+          ;; top level headlines in other agenda files
+          (org-agenda-files :maxlevel . 9)))
 
-(setq org-outline-path-complete-in-steps nil)
-(setq org-refile-use-outline-path t)
+  (setq org-outline-path-complete-in-steps nil)
+  (setq org-refile-use-outline-path t)
 
-;; Open agenda view in current window
-(setq org-agenda-window-setup (quote current-window))
+  ;; Open agenda view in current window
+  (setq org-agenda-window-setup (quote current-window))
 
-;; Don't show tasks as scheduled if deadline is shown
-(setq org-agenda-skip-scheduled-if-deadline-is-shown t)
+  ;; Don't show tasks as scheduled if deadline is shown
+  (setq org-agenda-skip-scheduled-if-deadline-is-shown t)
 
-;; TODO faces
-(setq org-todo-keyword-faces
-      '(("TODO" :foreground "#dc322f" :weight bold)
-        ("DONE" :foreground "#859900" :weight bold)
-        ("WIP" :foreground "#b58900" :weight bold)
-        ("BLOCKED" :foreground "#cb4b16" :weight bold)
-        ("PARKED" :foreground "#586e75" :weight bold)
-        ("IMPROV" :foreground "#268bd2" :weight bold)))
+  (setq org-agenda-custom-commands
+        '(("w" "Agenda view for work" agenda ""
+           ((org-agenda-category-filter-preset '("+work"))
+            (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("PARKED")))))
+          ("p" "Agenda view for personal" agenda ""
+           ((org-agenda-category-filter-preset '("+gtd"))))))
 
-;; enable export to markdown
-(eval-after-load "org"
-  '(require 'ox-md nil t))
+  ;;---------------------------------------------------------
 
+  ;; org crypt related config
+  (require 'org-crypt)
+  (org-crypt-use-before-save-magic)
+  (setq org-tags-exclude-from-inheritance '("crypt"))
+  (setq org-crypt-key "4096R/4930D5CB")
 
-(setq org-agenda-custom-commands
-      '(("w" "Agenda view for work" agenda ""
-         ((org-agenda-category-filter-preset '("+work"))
-          (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("PARKED")))))
-        ("p" "Agenda view for personal" agenda ""
-         ((org-agenda-category-filter-preset '("+gtd"))))))
+  ;;---------------------------------------------------------
 
+  ;; global key bindings
+  (global-set-key "\C-cl" 'org-store-link)
+  (global-set-key "\C-cc" 'org-capture)
+  (global-set-key "\C-ca" 'org-agenda)
+  (global-set-key "\C-cb" 'org-iswitchb)
+  (global-set-key "\C-co" 'org-mark-ring-goto)
+  (global-set-key (kbd "s-<f11>") 'org-clock-goto)
 
-(add-hook 'org-mode-hook (lambda () (hl-line-mode 1)))
+  ;;---------------------------------------------------------
 
-(require 'org-crypt)
-(org-crypt-use-before-save-magic)
-(setq org-tags-exclude-from-inheritance '("crypt"))
-(setq org-crypt-key "4096R/4930D5CB")
+  :hook
+  (org-mode . (lambda () (hl-line-mode 1))))
 
 (use-package org-roam
   :ensure t
