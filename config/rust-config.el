05079de3b926829726492905d1f411dceba2565b
Vineet Naik <naikvin@gmail.com>
2023-10-03 16:33:19 +0530

    Inhibit some annoying capabilities of eglot in rust-mode
    Namely, variable highlighting at point, inline type inference,
    function signature help. These make the editing experience rather
    "jumpy"! So I find it useful to disable them.
    
    Also enable company-mode for rust.

--- config/rust-config.el
+++ config/rust-config.el
@@ -7,8 +7,15 @@
 (use-package rust-mode
   :ensure t
 
+  :config
+  (setq eglot-ignored-server-capabilities
+        '(:documentHighlightProvider
+          :signatureHelpProvider
+          :inlayHintProvider))
+
   :hook
   (rust-mode . eldoc-mode)
+  (rust-mode . company-mode)
   ;; @TODO: Check if the current file is in a typical rust project dir
   ;; (using cargo) and initialize eglot only in that case. This is to
   ;; allow creating random rustc files which can be simply compiled
