edfc89c00295fb2aa23df90276c8c31202bcecd1
Vineet Naik <naikvin@gmail.com>
2023-10-21 22:46:29 +0530

    Fix package archives and remove bootstrapping of use-package
    In newer versions of emacs, bootstrapping of the 'use-package' package
    redundant and hence been removed.
    
    The package-archives are added using 'add-to-list' instead of 'setq'
    so that the default values are also retained.
    
    The org package archive at 'orgmode.org/elpa' is a broken
    link (responds with 403).
    
    This considerably reduces the time it takes for emacs to startup.

--- pkg-init.el
+++ pkg-init.el
@@ -7,24 +7,9 @@
 
 
 ;; Set package archive sources
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-                         ;; ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/")))
-
-
-;; Bootstrap 'use-package'
-(eval-after-load 'gnutls
-  '(add-to-list 'gnutls-trustfiles "/etc/ssl/cert.pem"))
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
-(require 'bind-key)
-(setq use-package-always-ensure t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
 
 
 (provide 'pkg-init)
