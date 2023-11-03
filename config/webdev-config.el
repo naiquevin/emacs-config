bd8059e50b62deb1d7b89ea71cc8f1eb3b357410
Vineet Naik <naikvin@gmail.com>
2023-11-03 16:37:25 +0530

    Consolidate html and js config into a single file 'webdev-config'

--- config/webdev-config.el
+++ config/webdev-config.el
@@ -49,3 +49,16 @@
   (set-face-attribute 'web-mode-html-attr-name-face nil :foreground "#b58900")
   (set-face-attribute 'web-mode-html-attr-custom-face nil :foreground "#cb4b16")
   (set-face-attribute 'web-mode-html-attr-equal-face nil :foreground "#839496"))
+
+
+;; @TODO: Extend the following code so that a static var can be
+;; defined with a list of project paths and jinja2-mode should be
+;; enabled for all html files under that projet.
+(add-hook
+ 'find-file-hook
+ (lambda ()
+   (when (and (string= (file-name-extension (buffer-file-name)) "html")
+              (projectile-project-root)
+              (string-match-p (expand-file-name "~/blog/blog-src")
+                              (projectile-project-root)))
+     (jinja2-mode))))
