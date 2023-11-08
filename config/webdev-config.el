c3dbdc12008128d8780f453f5763e568a0d07537
Vineet Naik <naikvin@gmail.com>
2023-11-08 10:07:23 +0530

    Fix padding for script and style tags in web-mode
    This fixes indentation for javascript and css in vue files. Also
    confirmed that it doesn't break anything in regular html files.

--- config/webdev-config.el
+++ config/webdev-config.el
@@ -38,6 +38,8 @@
   :mode ("\\.html\\'" "\\.vue\\'")
   :config
   ;; Adjust indentation
+  (setq web-mode-style-padding 2)
+  (setq web-mode-script-padding 2)
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
