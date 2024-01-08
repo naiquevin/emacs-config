5f3c1319e6c8383b9ddd7b13dedec3b806e9279c
Vineet Naik <naikvin@gmail.com>
2024-01-08 17:05:41 +0530

    Remove unnecessary code for initializing packages at startup
    In emacs version 29, it's sufficient to just add to the list of
    package archives (if required).

--- pkg-init.el
+++ pkg-init.el
@@ -1,15 +1,5 @@
-;; Disclaimer! Following code to ensure that the required packages are
-;; installed at startup is stolen from emacs-prelude
-;; (https://github.com/bbatsov/prelude)
-
-(require 'cl-lib)
-(require 'package)
-
-
 ;; Set package archive sources
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-
 
 (provide 'pkg-init)
