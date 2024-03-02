fcbe5df0347089b9378b344437da9376d1b1d456
Vineet Naik <naikvin@gmail.com>
2024-03-02 19:01:15 +0530

    Configure sqlformat mode to pass args to pg_format executable

--- config/prog-config.el
+++ config/prog-config.el
@@ -64,6 +64,7 @@
   :ensure t
   :config
   (setq sqlformat-command 'pgformatter)
+  (setq sqlformat-args '("--extra-function" "/Users/vineet/.pg_format/functions.txt"))
   :bind (:map sql-mode-map ("C-c C-f" . sqlformat-buffer)))
 
 
