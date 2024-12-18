5544c0e6bfa19d49c067ba6d87b506c09bcbcdd5
Vineet Naik <naikvin@gmail.com>
2024-12-18 11:41:18 +0530

    Add windows friendly settings
    - Inhibit EOL conversion
    - set buffer-file-coding-system to 'utf-8-unix by default
    - Hide dos EOL chars by default (with ability to show if required)

--- win.el
+++ win.el
@@ -2,4 +2,31 @@
 
 (set-frame-font "Courier New 11")
 
+;; Windows EOL
+;;
+;; Ref: https://stackoverflow.com/a/750933
+(defun hide-dos-eol ()
+  "Hide ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
+
+(defun show-dos-eol ()
+  "Show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M ?\^M))
+
+(add-hook 'find-file-hook 'hide-dos-eol)
+
+;; @TODO: Check if this is really required?
+;;
+;; It was added while debugging the CRLF issues in autoloads
+(setq-default buffer-file-coding-system 'utf-8-unix)
+
+;; Don't add CRLF when editing files with LF
+;;
+;; Ref: https://stackoverflow.com/a/1050795
+(setq inhibit-eol-conversion t)
+
 (provide 'win)
