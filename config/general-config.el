2ce4b66f439ab7b8baf97d152b952d48d2024066
Vineet Naik <naikvin@gmail.com>
2024-12-21 11:40:49 +0530

    Fix coding system used when saving/writing files
    Earlier, the the 2 variables 'buffer-file-coding-system' and
    'coding-system-for-write' were being set. These caused the files to be
    also written with unix style line endings (LF) regardless of the line
    endings that emacs detected when reading the file.
    
    This is particularly a problem when working on a code base that has
    some files in the LF format and some in CRLF format, because always
    saves the file in LF format.
    
    The fix is to not set the above variables, so that while writing the
    file back to the disk, emacs retains the same formatting that it
    identified while reading it.

--- config/general-config.el
+++ config/general-config.el
@@ -31,10 +31,8 @@
 (set-terminal-coding-system 'utf-8)
 (set-clipboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
-(setq file-name-coding-system 'utf-8
-      buffer-file-coding-system 'utf-8
-      coding-system-for-write 'utf-8
-      default-process-coding-system '(utf-8 . utf-8))
+(setq file-name-coding-system 'utf-8)
+(setq default-process-coding-system '(utf-8 . utf-8))
 
 
 (global-whitespace-mode)
