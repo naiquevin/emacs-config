189be70bad96f29be8e8849bf68e4c4f759fba90
Vineet Naik <naikvin@gmail.com>
2026-06-03 17:37:44 +0530

    Move treesitter based config to a separate file

--- config/prog-config.el
+++ config/prog-config.el
@@ -12,20 +12,6 @@
 
 (add-hook 'c-mode-hook 'electric-pair-mode)
 
-;;;; Tree-sitter config (not working for some reason)
-;; (setq treesit-language-source-alist
-;;       '((python "https://github.com/tree-sitter/tree-sitter-python")))
-;;
-;; (setq major-mode-remap-alist
-;;       '((python-mode . python-ts-mode)))
-
-;; [tag: treesitter-language-sources]
-(setq treesit-language-source-alist
-      '((typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-        (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-        (kotlin "https://github.com/fwcd/tree-sitter-kotlin" "main")
-        (yaml "https://github.com/tree-sitter-grammars/tree-sitter-yaml" "master")))
-
 (use-package eldoc
   :after (diminish)
   :diminish eldoc-mode
